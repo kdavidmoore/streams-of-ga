f0169a6667dda63644c1e479e796a127ff5dad7a
Keith Moore <keith.m4@gmail.com>
2016-09-30 15:57:31 -0400

    added formatting for date and lat-lng columns

---
+++ assets/getData.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  $('#mainTable').DataTable({
+    ajax: {
+      "url": "http://localhost:3015/",
+      "dataSrc": "value.timeSeries"
+    },
+    columns: [
+      { data: "sourceInfo.siteName" },
+      { data: "sourceInfo.geoLocation.geogLocation.latitude" },
+      { data: "sourceInfo.geoLocation.geogLocation.longitude" },
+      { data: "variable.variableName" },
+      { data: "values[0].value[0].value" },
+      { data: "values[0].value[0].dateTime" }
+    ],
+    columnDefs: [
+      {
+        render: function(data, type, row) {
+          return data.toFixed(4);
+        },
+        targets: [1, 2] 
+      },
+      {
+       render: function(data, type, row) {
+          var sampled = new Date(data);
+          return sampled;
+        },
+        targets: [5] 
+      }]
+  });
+});
