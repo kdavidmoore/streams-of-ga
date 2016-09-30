5f9a9cf2ef76ccc08c917afcc47bba439e8eb70a
Keith Moore <keith.m4@gmail.com>
2016-09-30 16:03:47 -0400

    changed api url

--- assets/getData.js
+++ assets/getData.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('#mainTable').DataTable({
     ajax: {
-      "url": "http://localhost:3015/",
+      "url": "https://streams-of-ga.herokuapp.com:3015/",
       "dataSrc": "value.timeSeries"
     },
     columns: [
