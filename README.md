4146955df2a04241d1bb521e73d8dac7ed35ce44
Keith Moore <keith.m4@gmail.com>
2016-09-30 15:08:49 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Streams of Georgia
+
+To use the [USGS REST IV service](http://waterservices.usgs.gov/rest/) with DataTables, I had to build a proxy server. Therefore, this project includes both front-end and back-end components.
+
+## Built with
+Express
+jQuery
+[DataTables](https://datatables.net/)
+Bootstrap
---
+++ server/routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var http = require('http');
+
+// router.get('/', function(req, res, next) {
+//     res.json({ warning: 'No HUC code provided.' });
+// });
+
+router.get('/', function(req, res, next) {
+    // var huc_raw = encodeURI(req.params.huc);
+    // var huc = huc_raw.substring(0, s.indexOf('?'));
+    // console.log(huc);
+    // var huc = 03130001;
+
+    var API_URL = {
+        host: 'waterservices.usgs.gov',
+        path: '/nwis/iv/?format=json&huc=03130001&parameterCd=00060,00065'
+    };
+
+    http.request(API_URL, function(response) {
+        var str = '';
+        response.on('data', function(chunk) {
+            str += chunk;
+        });
+        response.on('end', function() {
+            res.json(JSON.parse(str));
+        });
+    }).end();
+});
+
+module.exports = router;
